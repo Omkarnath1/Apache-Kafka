5a90c6f901a547378ab04590bf8dd850a01f0ef0
omkar <panageomkarnath@gmail.com>
2023-10-26 14:58:24 +0530

    first commit

---
+++ src/main/java/com/example/DeliveryBoy/Config/KafkaConfig.java
@@ -0,0 +1,26 @@
+package com.example.DeliveryBoy.Config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+import static com.example.DeliveryBoy.Config.AppConstants.LOCATION_TOPIC_NAME;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public NewTopic topic() {
+
+
+        return TopicBuilder
+                .name(LOCATION_TOPIC_NAME)
+//                .partitions()
+//                .replicas()
+                .build();
+
+
+    }
+
+}
